84554b703a38d4bb73c5049408b8674028cb3b6b
Yukti Jhawar <yuktimj2002@gmail.com>
2024-05-09 23:41:49 +0530

    tailwind setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        rubik: ["Rubik", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-});
+}
