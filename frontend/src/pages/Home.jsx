6dcd3b3f24c74d9039e90014c3e45b04d55b3094
devansh0331 <devanshshrivastava07@gmail.com>
2024-05-09 23:24:27 +0530

    setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-tailwind/react";
+
+function Home() {
+  return <Button className="bg-black text-4xl">Home</Button>;
+}
+
+export default Home;
